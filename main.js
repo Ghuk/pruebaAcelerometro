18744e80dbce644146bdb4196311576b2b370ffd
Camilo A. Ramirez A <Camilo.1219@hotmail.com>
2018-03-20 14:00:51 -0500

    Se agrego la vibracion

--- main.js
+++ main.js
@@ -14,6 +14,10 @@ function deviceOrientationHandler (eventData) {
   document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
   document.getElementById("doDirection").innerHTML = Math.round(dir);
 
+  if(80<=tiltFB=<100){
+    vibrateSimple();
+  }
+
 
   var logo = document.getElementById("imgLogo");
   logo.style.webkitTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
@@ -21,4 +25,7 @@ function deviceOrientationHandler (eventData) {
   logo.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
 }
 
+function vibrateSimple() {
+  navigator.vibrate(200);
+}
 
