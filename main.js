75ff135cf99b087229c6147343b22b64adab4a46
Camilo A. Ramirez A <Camilo.1219@hotmail.com>
2018-03-20 14:08:15 -0500

    Se quito la vibracion  x 2

--- main.js
+++ main.js
@@ -14,10 +14,6 @@ function deviceOrientationHandler (eventData) {
   document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
   document.getElementById("doDirection").innerHTML = Math.round(dir);
 
-  if(80<=tiltFB=<100){
-    vibrateSimple();
-  }
-
 
   var logo = document.getElementById("imgLogo");
   logo.style.webkitTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
@@ -25,7 +21,4 @@ function deviceOrientationHandler (eventData) {
   logo.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
 }
 
-function vibrateSimple() {
-  navigator.vibrate(200);
-}
 
